0bc8d34044b78e0f525bdf28802e86488f04e7fd
Nasrin Asadli <nasrinasadli@Atillas-MacBook-Air.local>
2023-07-10 12:37:48 +0400

    fourth

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,22 @@
-$('.responsive').slick({
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+$(() => {
+    $('.slider').slick({
         infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
+        speed: 200,
+        autoplay: true,
+        autoplaySpeed: 2800,
         slidesToShow: 1,
         slidesToScroll: 1
-      }
+    });
+    document.querySelector('#InterestBtn').onclick = () => {
+        document.querySelector('#InterestBtn').className = "d-inline-block active";
+        document.querySelector('#NewsButton').className = "d-inline-block"
+        document.querySelector('.interest').className = "interest";
+        document.querySelector('.news').className = "news d-none";
     }
-  ]
-});
+    document.querySelector('#NewsButton').onclick = () => {
+        document.querySelector('#InterestBtn').className = "d-inline-block";
+        document.querySelector('#NewsButton').className = "d-inline-block active"
+        document.querySelector('.interest').className = "interest d-none";
+        document.querySelector('.news').className = "news";
+    }
+})
