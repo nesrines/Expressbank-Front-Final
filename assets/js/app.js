dcfd4f00a2a3991649701ad3b10519d839ffd76c
Nasrin F. Asadli <nasrinfa@code.local>
2023-07-11 16:05:27 +0400

    sixth

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,16 +7,25 @@ $(() => {
         slidesToShow: 1,
         slidesToScroll: 1
     });
-    document.querySelector('#InterestBtn').onclick = () => {
-        document.querySelector('#InterestBtn').className = "d-inline-block active";
-        document.querySelector('#NewsButton').className = "d-inline-block"
+    let btns = document.querySelectorAll(".buttons p");
+    for (let btn of btns) {
+        btn.addEventListener("click", () => {
+            btn.parentElement.querySelector(".active").classList.remove("active");
+            btn.classList.add("active");
+        })
+    }
+    document.querySelector('.interest-btn').addEventListener("click", () => {
         document.querySelector('.interest').className = "interest";
         document.querySelector('.news').className = "news d-none";
-    }
-    document.querySelector('#NewsButton').onclick = () => {
-        document.querySelector('#InterestBtn').className = "d-inline-block";
-        document.querySelector('#NewsButton').className = "d-inline-block active"
+    })
+    document.querySelector('.news-button').addEventListener("click", () => {
         document.querySelector('.interest').className = "interest d-none";
         document.querySelector('.news').className = "news";
-    }
+    })
+
+    let date = new Date();
+    let month = date.getMonth() + 1;
+    if (month < 10) {month = `0${month}`;}
+    let currentDate = `${date.getDate()}-${month}-${date.getFullYear()}`;
+    document.querySelector(".current-date").innerHTML = currentDate;
 })
