5637be7d538328f1c3118ded44d6e03f2b472a64
Nasrin Asadli <nasrinasadli@Atillas-MacBook-Air.local>
2023-07-14 20:56:14 +0400

    tenth

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,13 +4,29 @@ $(() => {
         {document.querySelector('header, header *').style.position = 'fixed';}
         else {document.querySelector('header, header *').style.position = 'static';}
     };
+    let sidebtn = document.querySelector('.side-menu-btn');
+    sidebtn.addEventListener('click', () => {
+        if (sidebtn.firstElementChild.src === 'https://expressbank.az/img/close_menu.svg') {
+            sidebtn.firstElementChild.src = 'https://expressbank.az/img/menu.svg';
+            document.querySelector('.nav-1, .nav-2').style.display = 'block';
+            document.querySelector('main').style.display = 'block';
+            document.querySelector('footer').style.display = 'block';
+        }
+        else {
+            sidebtn.firstElementChild.src = 'https://expressbank.az/img/close_menu.svg';
+            document.querySelector('.nav-1, .nav-2').style.display = 'none';
+            document.querySelector('main').style.display = 'none';
+            document.querySelector('footer').style.display = 'none';
+        }
+        document.querySelector('.side-menu').classList.toggle('d-none');
+    });
     let tabButtonsFunction = btn => {
         let button = document.querySelector(btn);
         button.addEventListener('click', () => {
-            button.parentElement.querySelector(".active").classList.remove("active");
-            button.classList.add("active");
+            button.parentElement.querySelector('.active').classList.remove('active');
+            button.classList.add('active');
             let div = document.querySelector(btn.replace('-btn', ''));
-            let siblingDivs = div.parentElement.querySelectorAll(':scope > :not(.buttons)');
+            let siblingDivs = div.parentElement.querySelectorAll(':scope > :not(.buttons, .my-btn, .exchange-alert)');
             for (let siblingDiv of siblingDivs) {
                 if (!(siblingDiv.className.includes('d-none')))
                 {siblingDiv.classList.add('d-none');}
@@ -18,7 +34,7 @@ $(() => {
             if (div.className.includes('d-none')) {div.classList.remove('d-none');}
         });
     };
-    let btns = document.querySelectorAll(".buttons p");
+    let btns = document.querySelectorAll('.buttons p');
     let btnClassNames = [];
     for (let btn of btns) {btnClassNames.push('.' + btn.classList[0]);}
     for (let btn of btnClassNames) {tabButtonsFunction(btn);}
--- assets/js/index.js
+++ assets/js/index.js
@@ -7,9 +7,35 @@ $(() => {
         slidesToShow: 1,
         slidesToScroll: 1
     });
+    let select = document.querySelector('select');
+    let percent = document.querySelector('#Percent');
+    let amountSlider = document.querySelector('#AmountSlider');
+    let amount = document.querySelector('#Amount');
+    let monthSlider = document.querySelector("#MonthSlider");
+    let month = document.querySelector("#Month");
+    let Calculate = () => {
+        let amountValue = parseFloat(amount.innerHTML);
+        total = amountValue + amountValue * (percent.innerHTML / 100);
+        document.querySelector('#Total').innerHTML = total + ' ' + 'AZN';
+        document.querySelector('#Monthly').innerHTML = (total / month.innerHTML).toFixed(2);
+    }
+    select.addEventListener('change', () => {
+        percent.innerHTML = select.value;
+        Calculate();
+    });
+    amountSlider.addEventListener('input', () => {
+        amountSlider.style.background = `linear-gradient(90deg, #f9c20a ${amountSlider.value / 1500}%, #e2e2e2 0)`;
+        amount.innerHTML = amountSlider.value;
+        Calculate();
+    });
+    monthSlider.addEventListener('input', () => {
+        monthSlider.style.background = `linear-gradient(90deg, #f9c20a ${monthSlider.value / 3.6}%, #e2e2e2 0)`;
+        month.innerHTML = monthSlider.value;
+        Calculate();
+    });
     let date = new Date();
-    let month = date.getMonth() + 1;
-    if (month < 10) {month = `0${month}`;}
-    let currentDate = `${date.getDate()}-${month}-${date.getFullYear()}`;
-    document.querySelector(".current-date").innerHTML = currentDate;
-})
+    let currentMonth = date.getMonth() + 1;
+    if (currentMonth < 10) {currentMonth = `0${currentMonth}`;}
+    let currentDate = `${date.getDate()}-${currentMonth}-${date.getFullYear()}`;
+    document.querySelector('.current-date').innerHTML = currentDate;
+});
